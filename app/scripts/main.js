7669b3bd5525e6ed454cf6feaa6c007794fb0314
Andrew <vdovichencko@gmail.com>
2015-05-16 16:01:35 +0300

    search implemented

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,8 @@
 /* jshint devel:true */
-console.log('store is '+ (store.enabled ? 'enabled':'disabled'));
+console.log('store is ' + (store.enabled ? 'enabled' : 'disabled'));
 
 /* STORE API
-* // Store 'marcus' at 'username'
+ * // Store 'marcus' at 'username'
  store.set('username', 'marcus')
 
  // Get 'username'
@@ -28,12 +28,108 @@ console.log('store is '+ (store.enabled ? 'enabled':'disabled'));
  store.forEach(function(key, val) {
  console.log(key, '==', val)
  })
-* */
+ * */
 
-
- var App = function(){
-
+var App = function () {
 
 }
+
+var Search;
+Search = function () {
+
+  var placeTerms = ['магазин', 'супермаркет', 'универсам'],
+      timeTerms = ['сегодня', ' завтра'],
+      actionTerms = ['привезти', 'доставить', 'доставк', 'принести', 'купить'],
+      objectTerms = ['молоко', 'молока'],
+
+      suggectionsList = {
+        place : {
+          title: 'Откуда?',
+          list : ['Ближайшего магазина', 'Любого магазина']
+        },
+        time  : {
+          title: 'Когда?',
+          list : ['Сегодня', ' Завтра', '21 мая', '22 мая']
+        },
+        action: {
+          title: 'Что сделать?',
+          list : ['Привезти', 'Забрать', 'Купить']
+        },
+        object: {
+          title: 'Что именно?',
+          list : ['Фрукты', 'Молоко', 'Воду']
+        }
+
+      },
+      log = console.log.bind(console, 'Search: ');
+
+  function check(searchTerm) {
+    searchTerm = searchTerm.toLowerCase();
+    log('searchTerm =', searchTerm);
+    function checkTerms(terms) {
+      return terms.some(function (term) {
+        return searchTerm.indexOf(term) >= 0;
+      });
+    }
+
+    var suggestions = [],
+        placeSelected,
+        timeSelected,
+        actionSelected,
+        objectSelected,
+        anySelected;
+
+    placeSelected = checkTerms(placeTerms);
+    timeSelected = checkTerms(timeTerms);
+    actionSelected = checkTerms(actionTerms);
+    objectSelected = checkTerms(objectTerms);
+
+    anySelected = placeSelected || timeSelected || actionSelected || objectSelected;
+
+    if (anySelected) {
+      if (!placeSelected) {
+        suggestions.push(suggectionsList.place);
+      }
+      if (!timeSelected) {
+        suggestions.push(suggectionsList.time);
+      }
+      if (!actionSelected) {
+        suggestions.push(suggectionsList.action);
+      }
+      if (!objectSelected) {
+        suggestions.push(suggectionsList.object);
+      }
+    } else {
+      var allTerms, suggectionTerms, filtered;
+
+      var filter = function filter(element) {
+        return element.indexOf(searchTerm) === 0;
+      };
+
+      allTerms = [placeTerms, timeTerms, actionTerms, ['молоко']];
+
+      suggectionTerms = allTerms.reduce(function (prev, curr) {
+        filtered = curr.filter(filter);
+        return prev.concat(filtered);
+      }, []);
+
+      log('suggectionTerms =', suggectionTerms);
+
+      if (suggectionTerms.length > 1) {
+        suggestions.push(suggectionTerms);
+      }
+
+      if (suggectionTerms.length === 1) {
+        suggestions = check(suggectionTerms[0]);
+        suggestions.title = suggectionTerms[0];
+      }
+    }
+    return suggestions;
+  }
+
+  return {check: check};
+};
+
+
 
 
